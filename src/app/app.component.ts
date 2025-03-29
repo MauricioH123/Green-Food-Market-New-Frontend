b210e51635913de68035f2c44b30a8d76fed0954
MauricioH <mauricioh12345678912@gmail.com>
2025-03-29 16:13:05 -0500

    creacion del componente sidebar, footer, ademas de crear la lista desplegable del sidebar y cambio de icono en el index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SiderbarComponent } from './layout/siderbar/siderbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, SiderbarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

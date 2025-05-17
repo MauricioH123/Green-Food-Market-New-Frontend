6ee60acb778691231e66c416529dd7fb3eaad7b6
MauricioH <mauricioh12345678912@gmail.com>
2025-05-16 23:21:49 -0500

    creacion de metodos para agregar el subtoral por productos y el total de la factura de los clientes

--- src/app/pages/facturas/molecules/crear-facturas/crear-facturas.component.ts
+++ src/app/pages/facturas/molecules/crear-facturas/crear-facturas.component.ts
@@ -6,10 +6,11 @@ import { Producto } from '../../../../models/producto';
 import { Pagos } from '../../../../models/pagos';
 import { FacturaRequest } from '../../../../models/factura-request';
 import { DetalleProducto } from '../../../../models/detalle-producto';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-crear-facturas',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CurrencyPipe],
   templateUrl: './crear-facturas.component.html',
   styleUrl: './crear-facturas.component.css'
 })
@@ -23,6 +24,7 @@ export class CrearFacturasComponent implements OnInit, OnChanges {
   @Input() Pagos!:Pagos[];
   @Output() factura = new EventEmitter<FacturaRequest>();
 
+  totalFactura!:number;
 
   facturaForm = new FormGroup({
     cliente: new FormControl('', Validators.required),
@@ -34,6 +36,7 @@ export class CrearFacturasComponent implements OnInit, OnChanges {
         idProducto: new FormControl(null, Validators.required),
         cantidad: new FormControl(1, [Validators.required, Validators.min(1)]),
         precio: new FormControl(0, [Validators.required, Validators.min(0.01)]),
+        total: new FormControl(0, Validators.required),
       })
     ]),
     tipo_pago_id: new FormControl('',[Validators.required]),
@@ -67,8 +70,11 @@ export class CrearFacturasComponent implements OnInit, OnChanges {
       idProducto: new FormControl(null, Validators.required),
       cantidad: new FormControl(1, [Validators.required, Validators.min(1)]),
       precio: new FormControl(0, [Validators.required, Validators.min(0.01)]),
+      total: new FormControl(0, Validators.required),
+
     });
     this.productoss.push(nuevoProducto);
+    this.obtenerTotalPorProducto();
   }
 
   eliminarProducto(index:number){
@@ -104,11 +110,37 @@ export class CrearFacturasComponent implements OnInit, OnChanges {
     return undefined;
   }
 
+  obtenerTotalPorProducto(){
+    this.productoss.controls.forEach((element, index) => {
+      let productos = this.productoss.at(index);
+    productos.get('cantidad')?.valueChanges.subscribe( cantidad =>{
+      let precio = productos.get('precio')?.value;
+      productos.get('total')?.setValue(cantidad * precio);
+    })
 
+    productos.get('precio')?.valueChanges.subscribe( precio =>{
+      let cantidad = productos.get('cantidad')?.value;
+      productos.get('total')?.setValue(precio * cantidad);
+    })
+    });
+  }
+
+  obtenerTotalFactura(){
+
+    this.productoss?.valueChanges.subscribe(productos => {
+      const total = productos.reduce((acc: number, p: any) =>{
+        const subtotal = parseFloat(p.total) || 0;
+        return acc + subtotal;
+      }, 0);
+      this.totalFactura = total;
+    })
+  }
 
 
   ngOnInit(): void{
     this.enviarNombreCliente();
+    this.obtenerTotalPorProducto();
+    this.obtenerTotalFactura();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
